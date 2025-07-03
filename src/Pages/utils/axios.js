0db8a9d01d37a8f3c390f4f2ed6037c66e4cfc0c
Avinash Kushwaha <avikushwaha121@gmail.com>
2025-07-03 13:01:53 +0530

    url change

--- src/Pages/utils/axios.js
+++ src/Pages/utils/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "https://renteze.onrender.com",
-  baseURL: "http://localhost:3000",
-  // baseURL: "https://phpstack-501953-5654745.cloudwaysapps.com",
+  // baseURL: "http://localhost:3000",
+  baseURL: "https://phpstack-501953-5654745.cloudwaysapps.com",
 
 
   withCredentials: true,
